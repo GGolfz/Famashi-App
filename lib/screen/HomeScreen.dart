80af2c1b86470abb2bb3733f5327c8a3de31de9a
GGolfz <wisarutgolf11@gmail.com>
2021-05-10 22:36:43 +0700

    add: template layout :sparkles:

--- lib/screen/HomeScreen.dart
+++ lib/screen/HomeScreen.dart
@@ -1,17 +1,14 @@
-import 'package:famashi/widget/layout/appbar.dart';
-import 'package:famashi/widget/layout/drawer.dart';
+import 'package:famashi/widget/layout/template.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
   static String routeName = '/';
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text("Home Screen"),
-      ),
-      appBar: FamashiAppbar(title: 'Home'),
-      drawer: FamashiDrawer(),
-    );
+    return TemplateLayout(
+        child: Center(
+          child: Text("Home Screen"),
+        ),
+        title: 'Home');
   }
 }
