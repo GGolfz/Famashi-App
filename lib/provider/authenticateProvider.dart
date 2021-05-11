ad063b1953e9c76891326dccc40d7f88775aee9c
GGolfz <wisarutgolf11@gmail.com>
2021-05-11 22:28:30 +0700

    add: login, register, auto login, logout provider :sparkles:

--- lib/provider/authenticateProvider.dart
+++ lib/provider/authenticateProvider.dart
@@ -1,3 +1,5 @@
+import 'package:dio/dio.dart';
+import 'package:famashi/config/api.dart';
 import 'package:flutter/foundation.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -15,17 +17,52 @@ class AuthenticateProvider with ChangeNotifier {
   }
 
   Future<void> login(String email, String password) async {
-    _token = "Simple Token";
-    notifyListeners();
+    final prefs = await SharedPreferences.getInstance();
+    try {
+      final response = await Dio().post(apiEndpoint + '/auth/login',
+          data: {"email": email, "password": password});
+      _token = response.data["token"];
+      prefs.setString('userToken', _token!);
+      notifyListeners();
+    } on DioError catch (error) {
+      print(error.response!.data);
+    }
   }
 
   Future<void> register(String email, String password) async {
-    _token = "Simple Token";
-    notifyListeners();
+    final prefs = await SharedPreferences.getInstance();
+    try {
+      final response = await Dio().post(apiEndpoint + '/auth/register',
+          data: {"email": email, "password": password});
+      _token = response.data["token"];
+      prefs.setString('userToken', _token!);
+      notifyListeners();
+    } on DioError catch (error) {
+      print(error.response!.data);
+    }
   }
 
   Future<void> logout() async {
     _token = null;
     notifyListeners();
+    final prefs = await SharedPreferences.getInstance();
+    prefs.clear();
+  }
+
+  Future<void> tryAutoLogin() async {
+    final prefs = await SharedPreferences.getInstance();
+    if (!prefs.containsKey('userToken')) return;
+    final token = prefs.getString('userToken');
+    _token = token;
+    notifyListeners();
+    try {
+      await Dio().get(apiEndpoint + '/user',
+          options: Options(
+              headers: {"Authorization": "Bearer " + token.toString()}));
+    } catch (error) {
+      prefs.clear();
+      _token = null;
+      notifyListeners();
+    }
   }
 }
