2535372930669bbd7abe0bb3ec30a43631657f15
GGolfz <wisarutgolf11@gmail.com>
2021-05-12 11:58:29 +0700

    add: health info tab selector :sparkles:

--- lib/screen/HealthInfoScreen.dart
+++ lib/screen/HealthInfoScreen.dart
@@ -1,14 +1,64 @@
+import 'package:famashi/config/constant.dart';
+import 'package:famashi/config/style.dart';
+import 'package:famashi/widget/health/tabList.dart';
 import 'package:famashi/widget/layout/template.dart';
 import 'package:flutter/material.dart';
+import 'package:niku/widget/base.dart';
+import 'package:niku/widget/text.dart';
 
-class HealthInfoScreen extends StatelessWidget {
+class HealthInfoScreen extends StatefulWidget {
   static String routeName = '/healthinfo';
+
+  @override
+  _HealthInfoScreenState createState() => _HealthInfoScreenState();
+}
+
+enum TabType { Allergies, General, Medication }
+
+class _HealthInfoScreenState extends State<HealthInfoScreen> {
+  final _tabs = [
+    {"label": "Allergies", "type": TabType.Allergies},
+    {"label": "General Info", "type": TabType.General},
+    {"label": "Medication Info", "type": TabType.Medication}
+  ];
+  var _selectedTab = TabType.Allergies;
+
+  Widget getScreenBody() {
+    switch (_selectedTab) {
+      case TabType.Allergies:
+        return Center(
+          child: NikuText("Allergies"),
+        );
+      case TabType.General:
+        return Center(
+          child: NikuText("General Info"),
+        );
+      case TabType.Medication:
+        return Center(
+          child: NikuText("Medication Info"),
+        );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return TemplateLayout(
-        child: Center(
-          child: Text("Health Info Screen"),
-        ),
-        title: 'Health Info');
+        child: Container(
+            padding: EdgeInsets.all(kSizeS),
+            child: Column(
+              children: [
+                NikuText("My Health Information").style(kBody01Semibold),
+                kSizedBoxVerticalS,
+                TabList(
+                    tabs: _tabs,
+                    selected: _selectedTab,
+                    changeType: (tab) {
+                      setState(() {
+                        _selectedTab = tab;
+                      });
+                    }),
+                getScreenBody()
+              ],
+            )));
   }
 }
