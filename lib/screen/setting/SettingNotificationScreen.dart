335e0b2d1126ea7cb79a3f0b4c51f5bac1c7782d
GGolfz <wisarutgolf11@gmail.com>
2021-05-13 18:26:06 +0700

    add: profile & notify using template :sparkles:

--- lib/screen/setting/SettingNotificationScreen.dart
+++ lib/screen/setting/SettingNotificationScreen.dart
@@ -1,13 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:famashi/widget/layout/template.dart';
+import 'package:famashi/widget/utils/primaryButton.dart';
+import 'package:famashi/config/constant.dart';
+import 'package:niku/widget/axis.dart';
+import 'package:niku/widget/base.dart';
 
 class SettingNotificationScreen extends StatelessWidget {
   static String routeName = '/setting-notification';
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text("Setting Notification Screen"),
-      ),
+    return TemplateLayout(
+      child: Niku(Form(
+              key: _formKey,
+              child: NikuColumn([
+                Expanded(child: NikuColumn([]).crossStart()),
+                PrimaryButton(text: "Save", onPressed: () {})
+              ])))
+          .padding(EdgeInsets.only(
+              top: kSizeS, bottom: kSizeM, left: kSizeM, right: kSizeM)),
+      title: 'Notification Setting',
+      isHome: false,
     );
   }
 }
