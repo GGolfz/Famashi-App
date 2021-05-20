b17b2e22815adea06bb65de2fdff4b238cf26a87
GGolfz <wisarutgolf11@gmail.com>
2021-05-20 21:00:59 +0700

    change: calendar color :sparkles:

--- lib/config/theme.dart
+++ lib/config/theme.dart
@@ -6,18 +6,19 @@ final famashiTheme = _buildMainTheme();
 ThemeData _buildMainTheme() {
   final baseTheme = ThemeData.light();
   return baseTheme.copyWith(
-    accentColor: kAccentColor01,
-    primaryColor: kPrimaryColor01,
-    buttonTheme: baseTheme.buttonTheme.copyWith(
-        buttonColor: kPrimaryColor04,
-        colorScheme: baseTheme.colorScheme.copyWith(secondary: kAccentColor03)),
-    scaffoldBackgroundColor: kNeutralWhite,
-    textTheme: baseTheme.textTheme.copyWith(
-      headline1: kBody01Semibold,
-      headline2: kBody02Semibold,
-    ),
-    primaryIconTheme: baseTheme.iconTheme.copyWith(color: Colors.white),
-    inputDecorationTheme: InputDecorationTheme(
-        border: OutlineInputBorder(), focusColor: kAccentColor04),
-  );
+      accentColor: kAccentColor01,
+      primaryColor: kPrimaryColor01,
+      buttonTheme: baseTheme.buttonTheme.copyWith(
+          buttonColor: kPrimaryColor04,
+          colorScheme:
+              baseTheme.colorScheme.copyWith(secondary: kAccentColor03)),
+      scaffoldBackgroundColor: kNeutralWhite,
+      textTheme: baseTheme.textTheme.copyWith(
+        headline1: kBody01Semibold,
+        headline2: kBody02Semibold,
+      ),
+      primaryIconTheme: baseTheme.iconTheme.copyWith(color: Colors.white),
+      inputDecorationTheme: InputDecorationTheme(
+          border: OutlineInputBorder(), focusColor: kAccentColor04),
+      colorScheme: ColorScheme.light().copyWith(primary: kPrimaryColor04));
 }
