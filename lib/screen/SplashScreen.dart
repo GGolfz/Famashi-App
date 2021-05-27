cd3ee13985c4bb276c491577334be9caaeaf56ab
GGolfz <wisarutgolf11@gmail.com>
2021-05-27 13:13:17 +0700

    add: mockup lottie animation :sparkles:

--- lib/screen/SplashScreen.dart
+++ lib/screen/SplashScreen.dart
@@ -1,8 +1,10 @@
 import 'dart:async';
 
-import 'package:famashi/config/logo.dart';
+import 'package:famashi/config/constant.dart';
 import 'package:famashi/screen/HomeScreen.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:niku/widget/base.dart';
 
 class SplashScreen extends StatelessWidget {
   @override
@@ -11,9 +13,9 @@ class SplashScreen extends StatelessWidget {
       Navigator.of(context).pushNamed(HomeScreen.routeName);
     });
     return Scaffold(
-      body: Center(
-        child: kLongLogo2,
-      ),
+      body: Niku(
+        Lottie.asset('assets/animation/FamashiLogo.json'),
+      ).center().padding(EdgeInsets.all(kSizeM)),
     );
   }
 }
