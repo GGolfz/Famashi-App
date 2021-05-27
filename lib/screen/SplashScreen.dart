525f32eca4168dd0bd5b388ce89ee3e455bcb8f1
GGolfz <wisarutgolf11@gmail.com>
2021-05-27 23:50:44 +0700

    add: no repeat to animation

--- lib/screen/SplashScreen.dart
+++ lib/screen/SplashScreen.dart
@@ -9,12 +9,12 @@ import 'package:niku/widget/base.dart';
 class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    Timer(Duration(seconds: 2, milliseconds: 800), () {
-      Navigator.of(context).pushNamed(HomeScreen.routeName);
+    Timer(Duration(seconds: 3, milliseconds: 200), () {
+      Navigator.of(context).pushReplacementNamed(HomeScreen.routeName);
     });
     return Scaffold(
       body: Niku(
-        Lottie.asset('assets/animation/FamashiLogo.json'),
+        Lottie.asset('assets/animation/FamashiLogo.json', repeat: false),
       ).center().padding(EdgeInsets.all(kSizeM)),
     );
   }
